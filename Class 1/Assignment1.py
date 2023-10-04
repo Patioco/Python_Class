2ef37d6617eb9352e4e4beff91d6af40056dfb9d
Patrick Onwujekwe <toochio@live.com>
2023-10-04 00:40:45 -0400

    Python Beginners' Class by Helen

---
+++ Class 1/Assignment1.py
@@ -0,0 +1,44 @@
+'''Python Assignment 1
+
+Note: this assignment will help you understand variables and data types.
+
+1.Print out "First Assignment on python"
+
+2.Read up on these data types and create two variables each storing these types of data
+a. Integer
+b. Float
+c. String
+d. Double
+e. Boolean
+
+3. Print out 5 of the variables you created in different lines
+
+4. Print 3 of any variables you created in the same line
+
+5. Make sure you add a comment on your code before writing the code for each number listed in this task
+'''
+#SOLUTIONS:
+print("First Assignment on python")
+#Integer assignment
+age = 20
+shoesize = 40
+#Float assignment
+height_in_feet = 5.7
+version = 2.0
+#string assignment
+first_name = "Helen"
+middle_name = "Ugoeze"
+#Double assignment
+float(4)
+float(5)
+#Boolean assignment
+True
+False
+#print 5 variables in different lines
+print(age)
+print(shoesize)
+print(height_in_feet)
+print(first_name)
+print(middle_name)
+#print 3 variables on same line
+print(first_name,middle_name,age)
---
+++ Class 1/intro.py
@@ -0,0 +1,11 @@
+print(2)
+print('hello world')    
+name = 'hello world'
+age = 30
+salary = 400.9
+print(salary)
+
+#declaring multiple variables
+x=y=z=50
+print(str(x) + str(y) + str(z))
+print('Helen is',age,'years old')
