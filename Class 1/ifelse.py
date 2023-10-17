053b73f15de56b35b18c334b1e31c6dae10c1d5b
Patrick Onwujekwe <toochio@live.com>
2023-10-16 23:55:06 -0400

    new commits

---
+++ Class 1/ifelse.py
@@ -0,0 +1,8 @@
+age = 30
+
+match age:
+    case 20:print('you are a teenager')
+    case 30:print('you are quite young')
+    case 40:print('you are getting older')
+    case 50:print('oops')
+    
